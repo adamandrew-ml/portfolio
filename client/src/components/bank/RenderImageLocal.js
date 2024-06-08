0ab4bc344816982033b451ca951bb08f3c0583db
adamandrew-ml <adam.steven.andrew@gmail.com>
2024-06-08 20:31:55 +0100

    created react app

--- client/src/components/bank/RenderImageLocal.js
+++
@@ -1,8 +0,0 @@
-const RenderImageLocal = (props) => {
-
-  return (
-    <img className={props.className} alt="Nothing here" id = {props.imageID} src={process.env.PUBLIC_URL + props.imagePath}/>
-  )
-}
-
-export default RenderImageLocal
